f00ab8a17d74b49d766ae290f27a3f25587f356a
Silewis37 <Silewis37@gmail.com>
2024-12-06 13:43:12 -0500

    JARVIS BACKUP 12 | 06 | 2024 [01:43]6 | 2024 [01:43]

---
+++ backup-jarvis.py
@@ -0,0 +1,41 @@
+# File Name: backup-jarvis.py
+# Author: Samuel Lewis
+
+#$ TO-DO List $#
+#[] TO-DO List Item
+
+#* Libraries *#
+
+import os
+import sys
+
+#* Custom Libraries *#
+
+#~ import custom made libraries here
+
+#^ Variables ^#
+
+#~ create and store variables here
+
+#& Functions &#
+
+#~ define and build functions here
+
+#= Classes =#
+
+#~ define and build classes here
+
+#! Main Program !#
+
+os.system("git commit")
+
+
+
+#- UNASSIGNED COLOR -#
+#| UNASSIGNED COLOR |#
+#? UNASSIGNED COLOR ?#
+#+ UNASSIGNED COLOR +#
+#: UNASSIGNED COLOR :#
+#; UNASSIGNED COLOR ;#
+#% UNASSIGNED COLOR %#
+#@ UNASSIGNED COLOR @#
